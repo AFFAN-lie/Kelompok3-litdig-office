f91f45bffa0c2ea112bf06e345cd6652c6c21344
Encep Rio Fadillah <riofadillah19@student.upi.edu>
2026-04-02 13:35:43 +0700

    Menambahkan fungsi konversi suhu
    Dari celcius ke kelvin

--- main.c
+++ main.c
@@ -1,38 +1,10 @@
-#include <stdio.h>
-
-int main() {
-    int pilihan;
-    char lanjut;
-
-    do {
-        // Tampilan Menu Utama
-        printf("\n====================================\n");
-        printf("   KALKULATOR KONVERSI SATUAN CLI   \n");
-        printf("====================================\n");
-        printf("Pilih 1-4:\n");
-        printf("1. Konversi Suhu (Celcius ke F/K)\n");
-        printf("2. Konversi Jarak (KM ke M/CM)\n");
-        printf("3. Konversi Berat (KG ke G/Pons)\n");
-        printf("4. Konversi Waktu (Jam ke M/D)\n");
-        printf("------------------------------------\n");
-        printf("Masukkan pilihan Anda: ");
-        scanf("%d", &pilihan);
-
-        // Memanggil fungsi berdasarkan pilihan
-        switch(pilihan) {
-            case 1: konversiSuhu(); break;
-            case 2: konversiJarak(); break;
-            case 3: konversiBerat(); break;
-            case 4: konversiWaktu(); break;
-            default: printf("\nPilihan tidak valid!\n");
-        }
-
-        // Fitur Looping
-        printf("\nApakah Anda ingin mencoba konversi lain? (y/n): ");
-        scanf(" %c", &lanjut); // Spasi sebelum %c untuk menangkap karakter 'enter'
-
-    } while (lanjut == 'y' || lanjut == 'Y');
-
-    printf("\nTerima kasih! Program selesai.\n");
-    return 0;
+void konversiSuhu() {
+   system("cls");
+   int celcius, kelvin;
+   printf("*****Fungsi konversi suhu dari celcius ke kelvin****");
+   printf("\nMasukan suhu dalam celcius: ");
+   scanf("%d", &celcius);
+   kelvin = celcius + 273;
+   printf("%d celcius = %d kelvin ", celcius, kelvin);
+   
 }
